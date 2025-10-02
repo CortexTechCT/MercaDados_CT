c950a902bec1087b8fead93a6b3456f9adf92494
Matheus Alves <Matheus.procopio@aluno.senai.br>
2025-10-02 16:54:01 -0300

    sparkles: feat: Subindo o leitura de produtos quase completo

--- MercaDadosCT/src/App.jsx
+++ MercaDadosCT/src/App.jsx
@@ -1,15 +1,17 @@
 import { useState } from 'react'
 import { CadastroFuncionario } from './pages/CadastroUsuario/cadastrousuario'
+import { Registro } from './pages/Leitura_Produtos/Registro'
 import './App.css'
-import {Login} from './pages/Login/login'
+import { Login } from './pages/Login/login'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
-  <CadastroFuncionario/>
+      <Registro />
+      {/* <CadastroFuncionario/> */}
+      {/* <Login /> */}
     </>
   )
 }
