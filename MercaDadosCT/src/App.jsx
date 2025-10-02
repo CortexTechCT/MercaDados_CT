1af5f47b98e3ecc4e553498649c9633db698071b
Isaac Borges Oliveira <isaac.b.oliveira@aluno.senai.br>
2025-10-02 16:45:25 -0300

    subindo as telas

--- MercaDadosCT/src/App.jsx
+++ MercaDadosCT/src/App.jsx
@@ -1,15 +1,23 @@
 import { useState } from 'react'
 import { CadastroFuncionario } from './pages/CadastroUsuario/cadastrousuario'
+import { AdmFuncionario } from './pages/AdmFuncionarios/admfuncionario'
 import './App.css'
 import {Login} from './pages/Login/login'
+import { LucroEGastos } from './pages/LucrosGastos/lucrosgastos'
+import { Home } from './pages/Home/home'
+import { GestaoEstoque } from './pages/GestaoEstoque/gestaoestoque'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
-  <CadastroFuncionario/>
+    {<GestaoEstoque/>}
+    {/* {<Home/>} */}
+    {/* {<LucroEGastos/>}      */}
+    {/* {<Login/>} */}
+{/* {<AdmFuncionario/>} */}
+  {/* <CadastroFuncionario/> */}
     </>
   )
 }
