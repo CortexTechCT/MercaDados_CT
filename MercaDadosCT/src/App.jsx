a7c796a6056c87bac745ab0a63abfbe99ec62fa5
Yasmin Brito <yasmin.b.costa7@aluno.senai.br>
2025-10-07 13:43:17 -0300

    feat: Ajustando tela de fornecedor

--- MercaDadosCT/src/App.jsx
+++ MercaDadosCT/src/App.jsx
@@ -1,10 +1,4 @@
 import { useState } from 'react'
-<<<<<<< HEAD
-//import { CadastroFuncionario } from './pages/CadastroUsuario/cadastrousuario'
-import './App.css'
-import { Fornecedores } from './pages/Fornecedores/fornecedores'
-//import {Login} from './pages/Login/login'
-=======
 import { CadastroFuncionario } from './pages/CadastroUsuario/cadastrousuario'
 import { AdmFuncionario } from './pages/AdmFuncionarios/admfuncionario'
 import './App.css'
@@ -12,25 +6,25 @@ import {Login} from './pages/Login/login'
 import { LucroEGastos } from './pages/LucrosGastos/lucrosgastos'
 import { Home } from './pages/Home/home'
 import { GestaoEstoque } from './pages/GestaoEstoque/gestaoestoque'
->>>>>>> 1af5f47b98e3ecc4e553498649c9633db698071b
+import { Fornecedores } from './pages/Fornecedores/fornecedores'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
+  return ( 
     <>
-<<<<<<< HEAD
+
     <Fornecedores/>
-  {/*<Login/>*/}
-  {/*<CadastroFuncionario/>*/}
-=======
-    {<GestaoEstoque/>}
+    {/*<Login/>*/}
+    {/*<CadastroFuncionario/>*/}
+    {/*<GestaoEstoque/>*/}
     {/* {<Home/>} */}
     {/* {<LucroEGastos/>}      */}
     {/* {<Login/>} */}
-{/* {<AdmFuncionario/>} */}
-  {/* <CadastroFuncionario/> */}
->>>>>>> 1af5f47b98e3ecc4e553498649c9633db698071b
+    {/* {<AdmFuncionario/>} */}
+    {/* <CadastroFuncionario/> */}
+
     </>
   )
 }
