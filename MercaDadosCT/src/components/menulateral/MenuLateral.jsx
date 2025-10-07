fe1c646745e53a3c2e1eb4daf7eb8a45e7c3d550
Isaac Borges Oliveira <isaac.b.oliveira@aluno.senai.br>
2025-10-07 11:26:05 -0300

    subindo a navegação das telas

--- MercaDadosCT/src/components/menulateral/MenuLateral.jsx
+++ MercaDadosCT/src/components/menulateral/MenuLateral.jsx
@@ -1,29 +1,26 @@
 import "./MenuLateral.css";
 import iconeCasa from '../../assets/imagem_2025-09-23_101000545-removebg-preview 1.png';
 import iconeSair from '../../assets/Vector.png';
+import { Link } from "react-router-dom";
 
 export const MenuLateral = () => {
   return (
     <aside className="menu-lateral">
-
       <img className="CASA" src={iconeCasa} alt="casa" />
-
       <nav>
         <ul>
-          <li>Gerenciamento</li>
-          <li className="ativo">Fornecedor</li>
-          <li>Estoque</li>
-          <li>Lucro e Gastos</li>
-          <li>Administração</li>
-          <li>Produtos</li>
-          <li >Cadastro novo</li>
+          <li><Link to="/Home">Gerenciamento</Link></li>
+          <li><Link to="/Fornecedores">Fornecedor</Link></li>
+          <li><Link to="/GestaoEstoque">Estoque</Link></li>
+          <li><Link to="/LucrosGastos">Lucro e Gastos</Link></li>
+          <li><Link to="/AdmFuncionarios">Administração</Link></li>
+          <li><Link to="/CadastroUsuario">Cadastro novo</Link></li>
         </ul>
       </nav>
-
       <div className="sair">
         <img src={iconeSair} alt="Sair" />
         <span>sair da conta</span>
       </div>
     </aside>
-  );
-};
+  )
+}
