754c8137caa9d3f54de7b7b203b63f77203d56cb
Isaac Borges Oliveira <isaac.b.oliveira@aluno.senai.br>
2025-10-16 13:38:05 -0300

    Subindo o consumo

--- MercaDadosCT/src/App.jsx
+++ MercaDadosCT/src/App.jsx
@@ -1,85 +1,22 @@
-<<<<<<< HEAD
-import { useState } from "react";
-import "./App.css";
-import Rotas from "./routes/Routes"
-
-// import {Registro} from "./pages/Leitura_Produtos/Registro"
-
-  
-=======
-<<<<<<< HEAD
-import { useState } from "react";
-import "./App.css";
-
-
-
-
-// import { CadastroFuncionario } from "./pages/CadastroUsuario/cadastrousuario";
-//import { AdmFuncionario } from "./pages/AdmFuncionarios/admfuncionario";
-// import { Login } from "./pages/Login/login";
-// import { LucroEGastos } from "./pages/LucrosGastos/lucrosgastos";
-// import { Home } from "./pages/Home/home";
-//  import { GestaoEstoque } from "./pages/GestaoEstoque/gestaoestoque";
- import { Cadastroproduto } from "./pages/CadastroProduto/cadastroproduto";
-// import { Fornecedores } from "./pages/Fornecedores/fornecedores";
-// import Rotas from "./routes/Routes"; // se for usar rotas futuramente
-
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-    {/* <GestaoEstoque/> */}
-    {/* <Fornecedores/> */}
-     {/* <AdmFuncionario/> */}
-<Cadastroproduto/>
-=======
-<<<<<<< HEAD
-import React from "react";
-import "./App.css";
-import { LeituraProdutos } from "./pages/Leitura_Produtos/LeituraProdutos";
-import { FormaDePagamento } from "./pages/FormaDePagamento/FormaDePagamento";
-
-function App() {
-  return (
-    <>
-      {/* Página principal */}
-      <LeituraProdutos />
-      {/* < FormaDePagamento/> */}
-=======
 
 import { useState } from "react";
 import "./App.css";
 import Rotas from "./routes/Routes"
 
->>>>>>> a9bb1511b6aa4c6212b49ed0875ae22f9c1bc2b3
+
 function App() {
 
 
 
   return ( 
-    <>
-<<<<<<< HEAD
-      <Rotas /> 
-      </>
-  )}
-  
-
-
-
-
-=======
+<>
       <Rotas/>
-<<<<<<< HEAD
- 
-=======
 
->>>>>>> f5c072692021760268630caf0d25aa32dd99e7c8
->>>>>>> d9a89bb51add40e46639175e29bcc16188b10516
->>>>>>> 8f28868e5a8d696a57a6f7a3f1184782b0e22c2c
+ 
+
     </>
   );
 } 
->>>>>>> a9bb1511b6aa4c6212b49ed0875ae22f9c1bc2b3
+
 
 export default App;
