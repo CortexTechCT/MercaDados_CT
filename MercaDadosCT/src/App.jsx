03a2d04862c29aee1e761f039eb439617de72c2c
Matheus Alves <Matheus.procopio@aluno.senai.br>
2025-10-23 08:13:49 -0300

    fix: corrigindo pull

--- MercaDadosCT/src/App.jsx
+++ MercaDadosCT/src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Routes, Route } from "react-router-dom";
 import { LeituraProdutos } from "./pages/LeituraProdutos/LeituraProdutos";
 import { FormaDePagamento } from "./pages/FormaDePagamento/FormaDePagamento";
@@ -11,32 +10,5 @@ function App() {
     </Routes>
   );
 }
-=======
-
-import React from "react";
-import { useState } from "react";
-
-import "./App.css";
-import Rotas from "./routes/Routes"
-
-
-function App() {
-
-
-
-
-  return ( 
-<>
-      <Rotas/>
-
- 
-
-    </>
-  );
-} 
-
-
- 
->>>>>>> 6137098c2118dc53a56bf05c1df9682bbac09d22
 
 export default App;
